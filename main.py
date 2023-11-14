6aa85a3026d993c8ca2e042b4e2b0bdbcc8a8ce0
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-11-14 11:00:28 +0100

    init

---
+++ main.py
@@ -0,0 +1,55 @@
+import string
+
+translation_dict = dict(
+    [('a', '.-'), ('b', '-...'), ('c', '-.-.'), ('d', '-..'), ('e', '.'), ('f', '..-.'), ('g', '--.'), ('h', '....'),
+     ('i', '..'), ('j', '.---'), ('k', '-.-'), ('l', '.-..'), ('m', '--'), ('n', '-.'), ('o', '---'), ('p', '.--.'),
+     ('q', '--.-'), ('r', '.-.'), ('s', '...'), ('t', '-'), ('u', '..-'), ('v', '...-'), ('w', '.--'), ('x', '-..-'),
+     ('y', '-.--'), ('z', '--..'), (' ', '//'), ('/', '-..-.'), ('-', '-....-'), ('.', '.-.-.-'), (',', '--..--'),
+     ("1", ".----"), ("2", "..---"), ("3", "...--"), ("4", "....-"), ("5", "....."), ("6", "-...."), ("7", "--..."),
+     ("8", "---.."), ("9", "----."), ("0", "-----")])
+
+alphabet = {}
+morse = {}
+
+for key, value in translation_dict.items():
+    alphabet[key] = value
+    morse[value] = key
+
+
+def encode(plaintext: str) -> str:
+    plaintext = plaintext.lower()
+    morse_text = ""
+
+    for char in plaintext:
+        if char == ' ':
+            morse_text += '/'
+            continue
+        morse_text += alphabet[char] + '/'
+
+
+    morse_text += "//"
+    return morse_text
+
+
+def decode(morse_text: str) -> str:
+    morse_array = morse_text.split("//")
+    morse_array.pop()
+    plain_text = ""
+
+    for i in range(len(morse_array)):
+
+        current_element = morse_array[i].split('/')
+
+        for elem in current_element:
+            plain_text += morse[elem]
+
+        if i != len(morse_array)-1:
+            plain_text += ' '
+
+    return plain_text
+
+print(decode(encode("Najit vsechny bugy je casto problem")))
+print(decode(encode("Uspet neni lehke, ale je to sladka tecka.")))
+print(decode(encode(
+    "Do translation dict jsou pridany vsechny specialni znaky a cisla, ktera budete potrebovat. To stejne nerikam o malych pismenech.")))
+print(encode("ab c"))  # .-/-...//-.-.///
